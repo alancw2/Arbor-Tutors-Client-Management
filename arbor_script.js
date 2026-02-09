4860a1265e282853fb10c898d2336d5ef9cbf715
alancw2 <alancw2@illinois.edu>
2026-02-09 15:19:05 -0600

    first  commit

---
+++ arbor_script.js
@@ -0,0 +1,21 @@
+//I'm gonna change the variable names once I figure out how to back up data for users but for now I'll just make it for personal use
+var tutorName = "Alan";
+var tutorLastName = "Ward";
+var tutorEmail = "alanward@example.com";
+
+//fill out tutor name
+document.getElementById("Field11").value = tutorName;
+
+
+
+//load data from json
+function loadData() {
+    return JSON.parse(localStorage.getItem())
+}
+
+//add client
+
+//remove client
+
+//add session
+
